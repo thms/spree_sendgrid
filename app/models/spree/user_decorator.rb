ac0401b8d8642d74baa27b0c34ac0454d28ceedb
Thomas Boltze <thomas.boltze@gmail.com>
2013-05-24 16:53:41 +0800

    Only set preferences for logged in users.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -7,21 +7,23 @@ Spree::User.class_eval do
   
   attr_accessible :preferred_newsletter_subscription, :preferred_availability_subscription, :preferred_new_product_subscription
   
-  # Check if the user has subscriped to the newsletter before signing up, if so, update his preference
+  # Check if the user has subscribed to the newsletter before signing up, if so, update his preference
   # retired with different logic, all new users are automatically subscribed to all subscriptions
   # after_create :check_newsletter_subscriptions
   
   after_create :create_subscriptions
   
   def create_subscriptions
-    if newsletter_subscripttion = Spree::NewsSubscription.newsletter.find_by_email(self.email)
-      self.preferred_newsletter_subscription = true
-      newsletter_subscription.update_attribute(:user_id, self.id)
-    else
-      Spree::NewsSubscription.create(:kind => 'newsletter', :email => self.email, :user_id => self.id)
-    end 
-    Spree::NewsSubscription.create(:kind => 'new_product', :email => self.email, :user_id => self.id)
-    Spree::NewsSubscription.create(:kind => 'availability', :email => self.email, :user_id => self.id)
+    unless self.email.ends_with?("example.net")
+      if newsletter_subscripttion = Spree::NewsSubscription.newsletter.find_by_email(self.email)
+        self.preferred_newsletter_subscription = true
+        newsletter_subscription.update_attribute(:user_id, self.id)
+      else
+        Spree::NewsSubscription.create(:kind => 'newsletter', :email => self.email, :user_id => self.id)
+      end 
+      Spree::NewsSubscription.create(:kind => 'new_product', :email => self.email, :user_id => self.id)
+      Spree::NewsSubscription.create(:kind => 'availability', :email => self.email, :user_id => self.id)
+    end
   end
   
   
